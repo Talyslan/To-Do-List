b40b9e8480d977840a9cf522205eda7b5774cba0
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-08-23 23:03:39 -0300

    atualizando html e css - responsividade

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const box = document.querySelector('.tasks');
+const box = document.querySelector('.box-task');
 
 function add() {
   const value = document.querySelector('#textTask').value;
   
   const task = {
-  feito: false,
-  texto: undefined,
+    feito: false,
+    texto: undefined,
   };
 
   task.texto = value;
 
   const text = `
-    <div class="t">
+    <div class="task">
         <input type="checkbox" />
         <p>${task.texto}</p>
         <span></span>

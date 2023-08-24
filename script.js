32838380a5e9b2a2b0d7e77747f5386b3de7e39a
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-08-24 00:04:24 -0300

    organizando o js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const box = document.querySelector('.box-task');
+const task_list = [];
 
-const task = () => {return {feito: false, texto: undefined}};
+// Funções para adicionar, remover e dizer se a task está feita
+const activies = {
+  add: function() {
+    const value = document.querySelector('#textTask').value;
 
-function add() {
-  const value = document.querySelector('#textTask').value;
+    const task = {check: false, text: value}
 
-  task.texto = value;
+    box.innerHTML += `
+      <div class="task">
+          <input type="checkbox" />
+          <p>${task.text}</p>
+          <span></span>
+      </div>
+      `;
 
-  const text = `
-    <div class="task">
-        <input type="checkbox" />
-        <p>${task.texto}</p>
-        <span></span>
-    </div>
-    `;
-
-  box.innerHTML += text;
+      task_list.push(task);
+      console.log(task_list);
+  },
+  
+  //VER EVENT LISTENER
+  remove: function() {},
+  checked: function() {}
 }
+
+// Visualização do ALL, PENDENT AND CHECKS

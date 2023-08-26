f030561e207157a5110c9c81227d21dfd9265a9a
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-08-26 01:22:27 -0300

    continuando

--- readme.md
+++ readme.md
@@ -21,3 +21,7 @@ Programa sendo feito a fim de nota de um projeto de Programação Orientada a Ob
 - Consulta no site <strong>"Hora de Codar"</strong> (<a href="https://horadecodar.com.br/como-verificar-se-um-checkbox-esta-checado-c-javascript-ou-jquery/" target="_blank">https://horadecodar.com.br/como-verificar-se-um-checkbox-esta-checado-c-javascript-ou-jquery/</a>) para identificar com JavaScript se um input do tipo checkbox está marcado ou não
 
 - Site: <strong>Metring</strong> (<a href="https://metring.com.br/como-obter-o-valor-de-um-select-em-javascript#google_vignette">https://metring.com.br/como-obter-o-valor-de-um-select-em-javascript#google_vignette</a>) para identificar qual o value selecionado do select/dropdown
+
+- Consulta no <strong>StackOverflow</strong>: <a href="https://pt.stackoverflow.com/questions/244111/selecionar-elemento-pai-com-javascript-sem-jquery">https://pt.stackoverflow.com/questions/244111/selecionar-elemento-pai-com-javascript-sem-jquery</a> para ver como acessar o elemento pai de um elemento HTML
+
+- Site: <strong>W3Schools</strong> (<a href="https://www.w3schools.com/jsref/prop_element_children.asp">https://www.w3schools.com/jsref/prop_element_children.asp</a>) para saber como acessar os elementos filhos de um elemento HTML
--- script.js
+++ script.js
@@ -1,27 +1,63 @@
+const checkbox = document.querySelectorAll("input[type='checkbox'");
+const valueInput = document.querySelector("input[type='text']");
 const box = document.querySelector('.box-task');
+const error = document.querySelector('#error');
 const task_list = [];
+
+// Funções úteis
+const clearInput = () => valueInput.value = "";
+const verify = () => {
+  if (valueInput.value == "") {
+    error.innerText = "Error! Write something";
+    return false;
+  }
+  else {
+    error.innerText = "";
+    return true;
+  }
+}
+
+// Criação da classe
+class Task {
+  constructor(text) {
+    this.completed = false;
+    this.text = text;
+    this.category = "all";
+  };
+
+  basesHTML() {
+    return `<div class="task">
+        <input type="checkbox" />
+        <p>${this.text}</p>
+        <span onclick="activies.remove(this)">X</span>
+    </div>`
+  };
+
+};
 
 // Funções para adicionar, remover e dizer se a task está feita
 const activies = {
   add: function() {
-    const value = document.querySelector('#textTask').value;
-
-    const task = {check: false, text: value}
-
-    box.innerHTML += `
-      <div class="task">
-          <input type="checkbox" />
-          <p>${task.text}</p>
-          <span>X</span>
-      </div>
-      `;
-
-      task_list.push(task);
-  },
+    if (verify()) {
+      const taskTemp = new Task(valueInput.value);
   
-  //VER EVENT LISTENER
-  remove: function() {},
-  checked: function() {}
-}
+      box.innerHTML += taskTemp.basesHTML();
+  
+      task_list.push(taskTemp);
+      console.log(task_list)
+      clearInput();
+    }
+  },
+  remove:(element) => element.parentNode.classList.add("delete"),
+  checked: function() {
 
+    for (const item of checkbox)
+      item.addEventListener("click", () => {
+        item.parentNode.children[1].classList.toggle("risk")
+      });
+    
+  }
+};
+
+activies.checked()
 // Visualização do ALL, PENDENT AND CHECKS
